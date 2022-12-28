c9d1979e6721e32b7237586b1ce824b3cd880058
Sachin (Ranganath) K <ranganathkulkarni20@gmail.com>
2022-12-28 18:37:07 +0530

    Update endPoints.js

--- src/endPoints/endPoints.js
+++ src/endPoints/endPoints.js
@@ -4,5 +4,5 @@
 // export const FETCH_USERS_API="http://localhost/e-lib-react/fetchUsers.php"
 
 //Global API
-export const USER_REGISTER="https://rnk-projects.000webhostapp.com/e-lib-react/fetchUsers.php"
-export const FETCH_USERS_API="https://rnk-projects.000webhostapp.com/e-lib-react/userRegister.php"
+export const USER_REGISTER="https://rnk-projects.000webhostapp.com/e-lib-react/userRegister.php"
+export const FETCH_USERS_API="https://rnk-projects.000webhostapp.com/e-lib-react/fetchUsers.php"
