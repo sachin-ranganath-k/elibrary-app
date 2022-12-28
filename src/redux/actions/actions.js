2ca00bbfb097f86352e9ab2bb2f21c7a69520a5f
Sachin (Ranganath) K <ranganathkulkarni20@gmail.com>
2022-12-28 18:23:56 +0530

    Update actions.js

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -24,7 +24,7 @@ export const fetchUsers = () => {
 export const submitUserRegisterData = (payload) => {
   return async function (dispatch) {
     await axios
-      .post(`${USER_REGISTER}`, payload)
+      .get(`${USER_REGISTER}`, payload)
       .then((res) => {
         dispatch({ type: SUBMIT_USER_REGISTER_DATA, payload });
         dispatch({ type: USER_REGISTER_SUCCESS, payload: "Success" });
